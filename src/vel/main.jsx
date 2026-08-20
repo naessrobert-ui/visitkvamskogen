1717406e7252fbb7dc971c4796ad682930479f9c
naessrobert-ui <naessrobert@gmail.com>
2026-08-20 14:06:40 +0200

    Legg til avstemming i digitalt styrerom
    Legger til avstemming som sakstype med frist, synlige stemmer, alternative forslag og støtte for formelt styrevedtak.

--- src/vel/main.jsx
+++ src/vel/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import VelApp from './VelApp.jsx';
+import PollEnhancer from './pollEnhancer.jsx';
 import './vel.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <VelApp />
+    <>
+      <VelApp />
+      <PollEnhancer />
+    </>
   </StrictMode>,
 );
